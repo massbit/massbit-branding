f544670b2d2fe6a9ea0a2776944fb1d0d258a83f
ChrisUser <cristiano-raimondi@tiscali.it>
2023-08-13 12:51:53 +0200

    feat: improve intro section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,51 @@
-import * as React from "react";
-import type { HeadFC, PageProps } from "gatsby";
-import "../styles/main.sass";
+import * as React from "react"
+import type { HeadFC, PageProps } from "gatsby"
+import "../styles/main.sass"
+import { StaticImage } from "gatsby-plugin-image"
 
 const pageStyles = {
   color: "#232129",
   padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
+}
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
-};
+}
 const headingAccentStyles = {
   color: "#663399",
-};
+}
 const paragraphStyles = {
   marginBottom: 48,
-};
+}
 const codeStyles = {
   color: "#8A6534",
   padding: 4,
   backgroundColor: "#FFF4DB",
   fontSize: "1.25rem",
   borderRadius: 4,
-};
+}
 const listStyles = {
   marginBottom: 96,
   paddingLeft: 0,
-};
+}
 const doclistStyles = {
   paddingLeft: 0,
-};
+}
 const listItemStyles = {
   fontWeight: 300,
   fontSize: 24,
   maxWidth: 560,
   marginBottom: 30,
-};
+}
 
 const linkStyle = {
   color: "#8954A8",
   fontWeight: "bold",
   fontSize: 16,
   verticalAlign: "5%",
-};
+}
 
 const docLinkStyle = {
   ...linkStyle,
@@ -52,7 +53,7 @@ const docLinkStyle = {
   display: `inline-block`,
   marginBottom: 24,
   marginRight: 12,
-};
+}
 
 const descriptionStyle = {
   color: "#232129",
@@ -60,7 +61,7 @@ const descriptionStyle = {
   marginTop: 10,
   marginBottom: 0,
   lineHeight: 1.25,
-};
+}
 
 const docLinks = [
   {
@@ -73,7 +74,7 @@ const docLinks = [
     url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
     color: "#8954A8",
   },
-];
+]
 
 const badgeStyle = {
   color: "#fff",
@@ -89,7 +90,7 @@ const badgeStyle = {
   top: -2,
   marginLeft: 10,
   lineHeight: 1,
-};
+}
 
 const links = [
   {
@@ -135,12 +136,28 @@ const links = [
       "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
     color: "#663399",
   },
-];
+]
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main className="main-page-container">
-      <div className="first-section-container"></div>
+      <div className="first-section-container">
+        <div className="first-section-container__background" />
+        <div className="first-section-container__white-cover" />
+        <div className="first-section-container__content-box">
+          <span className="heading-text main-title-text">Massbit Branding</span>
+          <div className="first-section-container__content-box__main-image-wrapper">
+            <StaticImage
+              src="../images/massbit_main_logo.png"
+              placeholder="blurred"
+              alt="Massbit"
+            />
+          </div>
+          <span className="heading-text subtitle-text">
+            Developing order in a world of chaos
+          </span>
+        </div>
+      </div>
       <div className="three-columns-row">
         <div className="placeholder_dot" />
         <div className="placeholder_dot" />
@@ -204,9 +221,9 @@ const IndexPage: React.FC<PageProps> = () => {
         src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
       />
     </main>
-  );
-};
+  )
+}
 
-export default IndexPage;
+export default IndexPage
 
-export const Head: HeadFC = () => <title>Massbit Branding</title>;
+export const Head: HeadFC = () => <title>Massbit Branding</title>
